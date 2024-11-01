dbed0ad82c481810d5b274dcaead869043ec5f59
michael tio <tio.michael2004@gmail.com>
2024-11-01 14:27:49 +0700

    tambahin gorm

--- index.go
+++ index.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"simple-go-rest/routes"
+	"simple-go-rest/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
+	utils.Connect();
+
 	router:= gin.Default();
 	routes.SetupRoutes(router);
 	router.Run("localhost:9090");

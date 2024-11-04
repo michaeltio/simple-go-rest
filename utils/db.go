5d1fce146771e238c77be28e16096bda401b998b
michael tio <tio.michael2004@gmail.com>
2024-11-04 17:28:50 +0700

    update add env

--- utils/db.go
+++ utils/db.go
@@ -1,23 +1,31 @@
 package utils
 
 import (
-    "log"
-    "os"
-    "simple-go-rest/models"
+	"log"
+	"os"
+	"simple-go-rest/models"
 
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
+    errenv := godotenv.Load()
+
+    if( errenv != nil){
+        log.Fatal("Error loading .env file")
+    }
+    
     dbHost := os.Getenv("DB_HOST")
     dbPort := os.Getenv("DB_PORT")
     dbUser := os.Getenv("DB_USER")
     dbPassword := os.Getenv("DB_PASSWORD")
     dbName := os.Getenv("DB_NAME")
 
+    // dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPassword + " dbname=" + dbName + " port=" + dbPort + " sslmode=disable TimeZone=Asia/Shanghai"
     dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPassword + " dbname=" + dbName + " port=" + dbPort + " sslmode=disable TimeZone=Asia/Shanghai"
     var err error
     DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
